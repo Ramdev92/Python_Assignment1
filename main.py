ee570932d5a708bfe5035387921c15b959881756
Ramdev92 <76549688+Ramdev92@users.noreply.github.com>
2021-01-27 10:46:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# Assignment on Python variables to describe a song attributes 
+# Senorita by Shawn Mendes and Camila Cabello have multible attributes
+# In this code snippet, each attribute of song are represented with variables
+
+""" 
+Songname, ReleasedDate, 
+RecordedDate, Genre, Director, 
+Label, VideoLink, Copyright and AwardsRecieved are strings. so represented in quotes
+"""
+
+SongName = "Senorita"
+ReleasedDate = "June 21, 2019"
+RecordedDate = "May 2019"
+Genre = "Latin pop"
+Singers = "Shawn Mendes and Camila Cabello"
+Director = "Dave Meyers" 
+Label = "Island"
+VideoLink = "https://www.youtube.com/watch?v=Pkh8UtuejGw"
+Copyright = "Island Records"
+AwardsRecieved = "MTV Video Music Award, BillBoard Music Award, People's Choice Award, Teen Choice Award, American Music Award and more"
+
+#Duration is mentioned in seconds which is an integer
+
+DurationInSeconds = 191
+
+print(SongName)
+print(ReleasedDate)
+print(RecordedDate)
+print(Genre)
+print(Singers)
+print(Director)
+print(Label)
+print(VideoLink)
+print(Copyright)
+print(AwardsRecieved)
+print(DurationInSeconds)
+
