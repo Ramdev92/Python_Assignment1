56a6413916a7a2ffd80999ca14a91180676ee070
Ramdev92 <76549688+Ramdev92@users.noreply.github.com>
2021-01-27 11:04:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,16 @@
 """ 
 Songname, ReleasedDate, 
 RecordedDate, Genre, Director, 
-Label, VideoLink, Copyright and AwardsRecieved are strings. so represented in quotes
+Label, VideoLink, Copyright and AwardsRecieved are variables of type string data type. so represented in quotes
 """
+# 'SongName' is the name of the variable
+# '=' is the assignment operator that we use to store value in a variable
+# "Senorita" is the value stored inside the variable 'SongName'
 
 SongName = "Senorita"
+
+# Similar to 'SongName' multiple variables are created and assigned values
+
 ReleasedDate = "June 21, 2019"
 RecordedDate = "May 2019"
 Genre = "Latin pop"
